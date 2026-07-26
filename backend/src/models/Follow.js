07cc45de48849ee1b9784849bead97fa3e99127c
chandan <vishwakarmachandan2005@gmail.com>
2026-07-26 16:07:36 +0530

    added follow and profile models to backend

---
+++ backend/src/models/Follow.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const followSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    profileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profile',
+        required:true,
+    },
+    followedAt: {
+        type: Date,
+        default: Date.now,
+        required: true,
+    },
+    captialAllocated: {
+        type: Number,
+        required: true,
+    }
+}, {timestamps: true})
+
+const Follow = mongoose.model('Follow', followSchema)
+export default Follow;
