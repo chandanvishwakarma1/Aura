caf891712c466f1fbb6caaefa5795076929d1d0b
chandan <vishwakarmachandan2005@gmail.com>
2026-08-05 15:35:20 +0530

    added fetchBatchMarketPrice for batch prices

--- trading-engine/src/utils/price.js
+++ trading-engine/src/utils/price.js
@@ -9,7 +9,21 @@ const getCurrentPrice = async(symbol) => {
         // console.log(liveSnapshot.regularMarketPrice)
         return liveSnapshot.regularMarketPrice;
         } catch (error) {
-        console.error(error);
+        console.error(`Failed fetching price for ${symbol}`,error);
+        return null
     }
 }
 export default getCurrentPrice;
+
+export const fetchBatchMarketPrices = async(symbols) => {
+    const nsSymbols = symbols.map(s => s + '.NS')
+    try {
+        const quotes = await yahooFinance.quote(nsSymbols, {return: 'object',fields: ['symbol','regularMarketPrice']})
+        console.log(quotes['INFY.NS'].regularMarketPrice)
+        return quotes
+    } catch (error) {
+        console.log(`Error fetch batch prices: `, error)
+        return null
+    }
+}
+fetchBatchMarketPrices(['RELIANCE', 'TCS', 'INFY'])
